000190de3e042461ea8e6df1937216f432ecb1a7
NinaDang97 <khanh.2dbt@gmail.com>
2018-03-18 14:36:45 +0200

    componentWillReceiveProps from APIFetch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './Components/Form';
+import ParticipantList from './Components/ParticipantList';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        rdmPartList: []
+    }
+  }
+  componentDidMount(){
+    fetch("https://randomuser.me/api/?results=20")
+    .then((response) => response.json())
+    .then((responseData) => {
+        let rdmPartList = responseData.results.map((element, i) => {
+          return {
+              id: i,
+              name: `${element.name.first} ${element.name.last}`,
+              email: element.email,
+              number: element.phone
+          }
+        });
+      this.setState({rdmPartList});
+    })
+    .catch((err) => alert(err));
+  }
+  
   render() {    
     return (
       <div className="App">
-        <div className="header"></div>
-        <div className="content">
-          <h2>List of participants</h2>
-          
-          {/* Form goes here */}
-          <Form />
-         
-        </div>
+          <div className="header">
+            <img src='logo.png' alt='logo' />
+          </div>
+          <div className="content">
+            <h2>List of participants</h2>            
+              <ParticipantList 
+                rdmPartList={this.state.rdmPartList}
+               />
+          </div>
       </div>
     );
   }
--- src/Components/ParticipantList.js
+++ src/Components/ParticipantList.js
@@ -3,9 +3,93 @@ import './ParticipantList.css';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faTrash from '@fortawesome/fontawesome-free-solid/faTrash';
 import faPencilAlt from '@fortawesome/fontawesome-free-solid/faPencilAlt';
-import ParticipantGenerator from './ParticipantGenerator';
+import faArrowDown from '@fortawesome/fontawesome-free-solid/faArrowDown';
 
 class ParticipantList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            number: '',
+            participants: []
+        }
+    }
+        
+    componentWillReceiveProps(props){
+        if(typeof props.rdmPartList != 'undefined'){
+            this.setState({participants: props.rdmPartList});
+        }
+    }
+
+    inputChanged = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    //handle validation
+    //name must not be undefined
+    validateName(name)
+    {
+        if(name !== ''){
+            return true;
+        } else{
+            alert("Invalid Name");
+            return false;
+        }
+    }
+
+    //valid email must contain some special characters
+    validateEmail(email) 
+    {
+    const validEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (email.match(validEmail) !== null)
+        {
+            return true;
+        } else{
+            alert("Invalid Email")
+            return false;
+        }
+        
+    }
+    //valid phone number must contain numeric characters and NANP
+    validatePhone(number)
+    {
+    const validPhone =/^[\]?[(]?[0-9]{3}[)]?[-\s\]?[0-9]{3}[-\s\]?[0-9]{4,6}$/;
+        if(number.match(validPhone) !== null){
+            return true;
+        } else{            
+            alert("Invalid Phone Number");
+            return false;
+        }
+    }
+
+    //Add new participant row function
+    btnPress = (event) => {
+        //generate unique id based on milliseconds
+        let id = Date.now();  
+        let participant = {
+            id,
+            name: this.state.name,
+            email: this.state.email,
+            number: this.state.number
+        }
+
+        if(this.validateName(participant.name) && this.validateEmail(participant.email) && this.validatePhone(participant.number)){
+            //after submit, input will be left blank
+            let name = '', email = '', number = '';
+
+            this.setState({
+                name, email, number,
+                participants: [...this.state.participants, participant]
+            })
+        }
+    }
+
+    deletePart = (deletedId) => {
+        let participants = this.state.participants.filter((val) => val.id !== deletedId);
+        this.setState({participants});
+    }
+
     render(){
         //icon edit, delete styling
         const icon = {
@@ -13,6 +97,9 @@ class ParticipantList extends Component{
             // height: 24,
             // width: 24
         }
+        const sort = (
+            <FontAwesomeIcon style={icon} icon={faArrowDown} />
+        )
         const pencil = (
             <FontAwesomeIcon style={icon} icon={faPencilAlt} />
         )          
@@ -20,36 +107,44 @@ class ParticipantList extends Component{
             <FontAwesomeIcon style={icon} icon={faTrash} />
         )
 
-       
         //looping participants list
-        let participant = this.props.participants.map((val) => (            
+        let participant = this.state.participants.map((val) => (            
             <tr key={val.id}>
-                <td>{val.name}</td>
+                <td >{val.name}</td>
                 <td>{val.email}</td>
                 <td>{val.number}</td>
-                <td><a>{pencil}</a><a onClick={() => this.props.deletePart(val.id)}>{trash}</a></td>
+                <td><a>{pencil}</a><a onClick={() => this.deletePart(val.id)}>{trash}</a></td>
             </tr>
         ));
+
         return(
             <div>
+                <table className="form">
+                    <tbody>
+                        <tr> 
+                            <td className="name"><input type="text" name="name" value={this.state.name} onChange={this.inputChanged} placeholder="Full name" /></td>
+                            <td className="email"><input type="email" name="email" value={this.state.email} onChange={this.inputChanged} placeholder="E-mail address" /></td>
+                            <td className="number"><input type="text" name="number" value={this.state.number} onChange={this.inputChanged} placeholder="Phone number" /></td>
+                            <td className="btn"><button onClick={this.btnPress}>Add new</button></td>
+                        </tr>
+                    </tbody>
+                </table>  
+    
                 <table className="table">
-                <thead>
-                    <tr>
-                        <th>Name<i className="fa fa-long-arrow-down"></i></th>
-                        <th>E-mail address</th>
-                        <th>Phone number</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <ParticipantGenerator deletePart={this.props.deletePart} /> 
-                <tbody>                    
-                    {participant}
-                </tbody>
-            </table>    
-                
-            
-          </div>
-          
+                    <thead>
+                        <tr>
+                            <th className="name">Name <a>{sort}</a></th>
+                            <th className="email">E-mail address</th>
+                            <th className="number">Phone number</th>
+                            <th className="btn"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {participant}
+                    </tbody>
+                    
+                </table> 
+            </div>
         );
     }
 }
